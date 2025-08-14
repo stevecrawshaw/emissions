b699ef925e460a610862c9791d5d9f8f866b692c
Steve Crawshaw <murrayjarvis@gmail.com>
2025-08-14 10:51:33 +0100

    lgp index update with comments

--- lgp_chart_data.sql
+++ lgp_chart_data.sql
@@ -1,4 +1,7 @@
 duckdb
+
+
+-- get WECA+ data from ODS
 CREATE OR REPLACE TABLE lep_transport_domestic_tbl AS 
 SELECT year(calendar_year) "Year" , la_ghg_sector Sector, SUM(territorial_emissions_kt_co2e) "Emissions KTCO2e", 'WECA+' Area
 FROM read_parquet('https://opendata.westofengland-ca.gov.uk/api/explore/v2.1/catalog/datasets/ca_la_ghg_emissions_sub_sector_ods_vw/exports/parquet?lang=en&refine=cauthnm%3A%22West%20of%20England%22&refine=la_ghg_sector%3A%22Transport%22&refine=la_ghg_sector%3A%22Domestic%22&facet=facet(name%3D%22la_ghg_sector%22%2C%20disjunctive%3Dtrue)&facet=facet(name%3D%22cauthnm%22%2C%20disjunctive%3Dtrue)&qv1=(calendar_year%3A%5B2014-01-01%20TO%202023-01-01%5D)&timezone=Europe%2FLondon')
@@ -9,6 +12,8 @@ DESCRIBE lep_transport_domestic_tbl;
 
 FROM lep_transport_domestic_tbl;
 
+-- get the UK data from the most recent CSV file published in July each year
+
  CREATE OR REPLACE TABLE uk_transport_domestic_tbl AS
  SELECT calendar_year "Year", la_ghg_sector Sector, sum(territorial_emissions_kt_co2e) "Emissions KTCO2e", 'UK' Area 
  FROM read_csv('data/2005-23-uk-local-authority-ghg-emissions-CSV-dataset.csv', normalize_names=true)
@@ -18,7 +23,7 @@ FROM lep_transport_domestic_tbl;
  GROUP BY calendar_year, la_ghg_sector
  ORDER BY calendar_year, la_ghg_sector;
 
-
+--  union the two tables
 CREATE OR REPLACE TABLE emissions_compare_base_tbl AS
 FROM uk_transport_domestic_tbl
 UNION BY NAME
@@ -26,13 +31,14 @@ FROM lep_transport_domestic_tbl;
 
 FROM emissions_compare_base_tbl;
 
-
-
+-- pivot to create columns by time
 CREATE OR REPLACE TABLE pivoted_emissions_base_tbl AS
 PIVOT emissions_compare_base_tbl
 ON Sector, Area
 USING sum("Emissions KTCO2e");
 
+
+-- create an indexed dataset for the chart in the powerpoint
 CREATE OR REPLACE TABLE indexed_ts_emissions_tbl AS 
 WITH base_year AS (
   SELECT
@@ -64,6 +70,8 @@ FROM
   base_year b
 ORDER BY
   d."Year";
+
+-- save to csv and use in the template slides PPT
 
 COPY indexed_ts_emissions_tbl TO
 'data/lgp_lep_uk_indexed_emissions.csv';
